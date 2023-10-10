1e38d825f7e602a7c40c11b1272d508060d2ba10
Yome  Ekoko <EkokoY@accessbankplc.com>
2023-10-10 11:38:39 +0100

    first commit

---
+++ RetailAccounts/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Net.Http;
+using System.Net.Http.Json;
+
+class program
+{
+    static void Main(string[] sargs)
+
+    {
+        String url = "http://10.111.23.13:7843/testaccountequiry/v1/GenerateRetailAccount";
+        var authTokenAlt = "TSTo4fA4qFFCTFswX7OrA==";
+        var merchantId = Console.ReadLine();
+        var channelCode = "TESTAPP";  
+        var payload=new { merchant_id = merchantId, channel_code = channelCode };   
+
+        var amount = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < amount; i++)
+        {            
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", authTokenAlt);
+            var response = client.PostAsJsonAsync(url,payload).Result;
+            response.Content.ReadAsStringAsync().Wait();
+            if (response.IsSuccessStatusCode)
+            {
+                var message = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            }
+
+
+            Console.WriteLine($"Called external service {amount} times for merchant {merchantId}");
+        }
+    }
+}
+
+
